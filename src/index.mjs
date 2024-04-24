f985826a873b83579e64770e5cb9dcb48e950576
jnthnexp <jnthnexpdev@gmail.com>
2024-04-23 21:32:13 -0600

    Update cors

--- src/index.mjs
+++ src/index.mjs
@@ -9,34 +9,44 @@ import router from './routes/index_routes.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigins = ['http://localhost:4200', 'https://itc-calificaciones.vercel.app'];
 
-const allowedOrigins = [
-    'http://localhost:4200',
-    'https://itc-calificaciones.vercel.app',
-];
-
-//Json config
+// Json config
 app.use(bodyParser.json({limit : '5mb'}));
 app.use(bodyParser.urlencoded({limit: '5mb', extended : true}));
 app.use(bodyParser.json());
 
-//Cors 
-app.use(cors({origin : allowedOrigins[1], credentials : true}));
+// Cores
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
-//Headers settings
+// No es necesario establecer los headers de CORS manualmente si estás utilizando el middleware `cors`
+// Sin embargo, si decides hacerlo, asegúrate de hacerlo correctamente:
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", allowedOrigins[1]);
+    if (allowedOrigins.includes(req.headers.origin)) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin); // Esta línea permite un origen específico
+    }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     next();
 });
 
+
 connectToMongoDB();
 
-//Router of routes api
+// Router of routes api
 app.use(router);
 
-//Listen port
+// Listen port
 app.listen(port, async () => {
     console.log(`API Server on port ${port}`);
 });
